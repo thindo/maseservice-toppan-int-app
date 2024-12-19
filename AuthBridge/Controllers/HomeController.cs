9479217f5acc89fb45c86e733018d22e1fa37708
Vi Diec <49938194+prayz212@users.noreply.github.com>
2024-12-19 16:30:39 +0700

    feat: add TODO

--- AuthBridge/Controllers/HomeController.cs
+++ AuthBridge/Controllers/HomeController.cs
@@ -13,13 +13,42 @@ namespace AuthBridge.Controllers
             _logger = logger;
         }
 
+        [HttpGet("/kah")]
         public IActionResult Index()
         {
+            // TODO: show MAS login page with button
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet("/kah/auth/oidc/callback")]
+        public IActionResult OIDCCallback()
         {
+            // TODO: Handle Toppan authentication callback [CHUAN]
+            // On Failure: redirect to error page
+            // On Success: process sending get user info request
+
+            // TODO: Send UserToken to get User Info [CHUAN]
+            // On Failure: redirect to error page
+            // On Success: process verify role
+
+            // TODO: Verify authorization role [THIN]
+            // On Failure: redirect to error page
+            // On Success: process check existing user
+
+            // TODO: Check existing user using NRIC FIN [THIN]
+            // On Existed: generate user session (KAH session)
+            // On Not Existed: process get user email
+
+                // TODO: Send get user email request [THIN]
+                // On Failure: retry 3 times => then follow On Success flow
+                // On Success: process create/update user => generate user session
+
+            // TODO: Create/update user [CHUAN]
+
+            // TODO: Generate user session
+
+            // TODO: Generate wsfed token???
+
             return View();
         }
 
